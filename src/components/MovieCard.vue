14c3eb2f145d8beab5311c2a8952781bdef07692
JeongChedah <donlawan.13@mail.kmutt.ac.th>
2021-04-15 15:05:21 +0700

    62130500026 moviecard component

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-gray-500 bg-opacity-50 shadow-lg rounded-lg p-2">
+        <slot></slot>
+    </div>
+</template>
--- src/views/Cenima.vue
+++ src/views/Cenima.vue
@@ -1,7 +1,9 @@
 <template>
+  <body-card>
     <div class="flex flex-col justify-start">
         <h1 class="font-semibold text-lg tracking-widest">
           Movies 
         </h1>
     </div>
+  </body-card>
 </template>
