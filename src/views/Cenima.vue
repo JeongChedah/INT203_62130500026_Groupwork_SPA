e221566a3a2ad4c9ba1f778a9721b95ad008827f
JeongChedah <donlawan.13@mail.kmutt.ac.th>
2021-04-16 15:57:06 +0700

    62130500026 Modify bgimg cenimapage

--- src/views/Cenima.vue
+++ src/views/Cenima.vue
@@ -1,8 +1,12 @@
 <template>
-  <movie-card class="w-screen ">
+  <movie-card>
+    <template v-slot:wallimg>
       <img src="../assets/background.jpg">
+    </template>
+    <template v-slot:text>
         <div class="flex flex-col justify-center items-center">
           <h1 class="text-white font-extrabold text-3xl tracking-widest">-Page not found -</h1>
         </div>
+    </template>
   </movie-card>
 </template>
