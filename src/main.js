1f1fb1bda40d39967d6e1768c41144ab13797dc6
JeongChedah <donlawan.13@mail.kmutt.ac.th>
2021-04-16 20:29:37 +0700

    62130500026 Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
 import MovieCard from './components/MovieCard.vue'
+import BodyCard from './components/BodyCard.vue'
 const app = createApp(App)
 app.component('movie-card', MovieCard)
+app.component('body-card', BodyCard)
 app.use(router).mount('#app')
