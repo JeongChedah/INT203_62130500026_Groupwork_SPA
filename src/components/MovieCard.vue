69efbed897782f5a62dc1663d686b8042011c2d8
JeongChedah <donlawan.13@mail.kmutt.ac.th>
2021-04-16 20:27:19 +0700

    62130500026 Modify tailwind in MovieCard Component

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,45 +1,40 @@
-<template>
-    <div class=" z-0"  style="background-color: #1b1b1b;">
-    <div class="content" >
+<template >
+    <div class="content z-0" >
         <slot name="text"></slot>
     </div>
-    <div id="bgimg">
-        <img class="bgpop" >
+    
+    <div id="bg-viewport">
         <slot name="wallimg"></slot>
+        <img class="bgpop" >
+        
     </div>
-</div>
+
 </template>
 <style>
     .bgpop {
-        display: block;
-  min-width: 10%;
-  min-height: 10%;
-  width: auto;
-  height: auto;
-  max-width: none;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  
-
+        
+        min-width: 10%;
+        min-height: 10%;
+        width: auto;
+        height: auto;
+        top: 0;
+        left: 0;
+        right: 0;
     }
-    #bgimg {
-        position: absolute;
-        margin-top:0;
+    #bg-viewport {
+        position: fixed;
         top: 0;
         left: 0;
         width: 100%;
         height: 100%;
-        z-index: 1;
+        z-index: -1;
         
     }
     .content {
         width: 100%;
         max-width: 100%;
         height: 100%;
-        margin: 0 ;
-        top: 0;
+        
         position: relative;
         z-index: 2;
     }
