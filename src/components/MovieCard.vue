3ab8512fd231be0d6164371af4cbda2f42c3010c
JeongChedah <donlawan.13@mail.kmutt.ac.th>
2021-04-16 15:57:51 +0700

    62130500026 Update Movie page

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,5 +1,46 @@
 <template>
-    <div class="bg-black shadow-lg p-2 " >
-        <slot></slot>
+    <div class=" z-0"  style="background-color: #1b1b1b;">
+    <div class="content" >
+        <slot name="text"></slot>
     </div>
+    <div id="bgimg">
+        <img class="bgpop" >
+        <slot name="wallimg"></slot>
+    </div>
+</div>
 </template>
+<style>
+    .bgpop {
+        display: block;
+  min-width: 10%;
+  min-height: 10%;
+  width: auto;
+  height: auto;
+  max-width: none;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  
+
+    }
+    #bgimg {
+        position: absolute;
+        margin-top:0;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 1;
+        
+    }
+    .content {
+        width: 100%;
+        max-width: 100%;
+        height: 100%;
+        margin: 0 ;
+        top: 0;
+        position: relative;
+        z-index: 2;
+    }
+</style>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,13 +1,18 @@
 <template>
     <movie-card>
-        <div class="m-5">
-            <li class="grid grid-cols-2 gap-y-6 ml-5">
-            <img class="w-auto h-auto mt-3" 
-            :src="require(`../assets/movies/${movie.src}`)"
-            />
-            <span class="text-white text-xl p-5 m-5">{{movie.name}}</span>
-            </li>
-        </div>
+        <template v-slot:text>
+            <div class="m-5">
+                <li class="grid grid-cols-2 gap-y-6 ml-5">
+                    <img class="w-auto h-auto mt-3" 
+                    :src="require(`../assets/movies/${movie.src}`)"
+                    />
+                    <span class="text-white text-xl p-5 m-5">{{movie.name}}</span>
+                </li>
+            </div>
+        </template>
+        <template v-slot:wallimg>
+            <img src="../assets/background.jpg">
+        </template>
     </movie-card>
 </template>
 <script>
