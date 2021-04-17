328189ab00ea315264a1ff1719c643764f159629
JeongChedah <donlawan.13@mail.kmutt.ac.th>
2021-04-17 15:07:43 +0700

    62130500026 Cenima page

--- src/views/Cenima.vue
+++ src/views/Cenima.vue
@@ -5,7 +5,7 @@
     </template>
     <template v-slot:text>
         <div class="flex flex-col justify-center items-center">
-          <h1 class="text-white font-extrabold text-3xl tracking-widest">-Page not found -</h1>
+          <h1 class="text-white font-extrabold text-3xl tracking-widest mt-56 ">-Page not found -</h1>
         </div>
     </template>
   </movie-card>
